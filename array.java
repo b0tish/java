e4cefb53d9d37ae0df203a98d611fd030546c229
b0tish <maharjan.saugat1@gmail.com>
2024-11-22 14:04:07 +0545

    initial commit

---
+++ array.java
@@ -0,0 +1,6 @@
+public class Array {
+  public static void main(String[] args){
+    String[] name = {"Anon1", "Anon2", "Anon3"};
+    System.out.println(name[0])
+  } 
+}
---
+++ example.java
@@ -0,0 +1,9 @@
+public class example {
+  public static void main(String[] args) {
+    int[] age = {20,21,19,16,10};
+
+    for (int i: age){
+      System.out.println(i);
+    }
+  } 
+}
